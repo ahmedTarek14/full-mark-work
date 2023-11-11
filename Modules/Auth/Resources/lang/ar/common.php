d796edfa87035be35ac2f1e5b8eee6e6e283e720
zamportta <ahmedtarek.am@gamil.com>
2023-11-11 13:37:01 +0200

    edited on auth

---
+++ Modules/Auth/Resources/lang/ar/common.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'account_closed' => 'تم إغلاق حسابك برجاء التواصل مع الإدارة',
+    'password_mismatch' => 'الرقم السري غير متطابق',
+    'data_check_failed' => 'برجاء مراجعة بيانات والمحاولة مرة أخرى',
+    'logout_successful' => 'تم تسجيل الخروج بنجاح',
+    'email' => 'البريد الإلكتروني',
+    'verification_code_sent' => 'تم إرسال الرمز التعريفي لبريدك الإلكتروني',
+    'verification_code' => 'الرمز التعريفي',
+    'new_password' => 'الرقم السري الجديد',
+    'no_matching_data' => 'لا توجد مثل هذه البيانات في قاعدة البيانات',
+    'invalid_verification_code' => 'الرمز التعريفي غير صحيح',
+    'password' => 'الرقم السري',
+    'old_password' => 'الرقم السري القديم',
+    'name' => 'الاسم بالكامل',
+    'Account_deleted_successfully' => 'تم حذف الحساب بنجاح',
+];
---
+++ Modules/Auth/Resources/lang/en/common.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'account_closed' => 'Your account has been closed. Please contact administration for assistance.',
+    'password_mismatch' => 'Password mismatch.',
+    'data_check_failed' => 'Please review your data and try again.',
+    'logout_successful' => 'Logout successful.',
+    'email' => 'Email',
+    'verification_code_sent' => 'A verification code has been sent to your email.',
+    'verification_code' => 'Verification Code',
+    'new_password' => 'New Password',
+    'no_matching_data' => 'No matching data found in the database.',
+    'invalid_verification_code' => 'Invalid verification code.',
+    'password' => 'Password',
+    'old_password' => 'Old password',
+    'name' => 'Full Name',
+    'Account_deleted_successfully' => 'Account deleted successfully',
+];
